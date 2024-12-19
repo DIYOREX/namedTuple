5359b1ad9db8224cf3a7b9116bd711ee17bc48a6
dev <dev@Diyorbeks-MacBook-Pro.local>
2024-12-19 19:33:09 +0500

    done

--- branch.py
+++ branch.py
@@ -1,5 +1,12 @@
-def (username, password):
-    if username == "admin" and password == "admin":
+
+def login(username, password):
+    if username == 'admin' and password == 'admin':
         return True
     else:
         return False
+
+
+
+def func():
+    return 'Hello World'
+
